d92b2c8930274a951266094dda8cd22ba29ba8ee
bralva-m <bralva.co15@gmail.com>
2022-10-18 09:15:23 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle"
- * Return : 0
+ * main - prints "Programming is like building a multilingual puzzule"
+ *
+ * return - if no errors prints (0)
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+puts("/"Programming is like building a multilingual puzzle"");
+return (0);
 }
