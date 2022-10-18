17a83f9e5dc7c95593184fef121e7d2e6bbb1be1
bralva-m <bralva.co15@gmail.com>
2022-10-18 09:24:22 -0700

    using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * prints the phrase using printf function
+ * return 0 if no errors
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n";
+  return (0);
+}
